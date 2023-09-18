5f896b66a382b92ffd7a9985f72de9fab9cb0633
Nicolas Stebner <nicolas_stebner@hotmail.com>
2023-09-18 19:59:08 -0300

    ahora si

--- server/db.ts
+++ server/db.ts
@@ -8,7 +8,7 @@ const serviceAccount = {
 	private_key_id: process.env.FIREBASE_KEY_ID,
 	private_key:
 		process.env.NODE_ENV === "production"
-			? process.env.FIREBASE_PRIVATE_KEY
+			? Buffer.from(process.env.FIREBASE_PRIVATE_KEY, "base64").toString("utf8")
 			: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
 	client_email: process.env.FIREBASE_CLIENT_MAIL,
 	client_id: process.env.FIREBASE_CLIENT_ID,
