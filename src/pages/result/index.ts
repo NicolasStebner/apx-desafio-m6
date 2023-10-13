1378e73030ba8a043ef39e49ec1e677211871e97
Nicolas Stebner <nicolas_stebner@hotmail.com>
2023-10-12 23:34:44 -0300

    new div tag for inverted box

--- src/pages/result/index.ts
+++ src/pages/result/index.ts
@@ -8,7 +8,10 @@ export async function initPageResult(params) {
 		contenedorManoUser!.style.transform = "translateY(-50px)";
 		contenedorManoUser!.style.transform = "rotate(360deg);";
 		contenedorManoUser!.style.transition = "all 1s ease-in-out";
-
+		var contenedorManoOpponent = div.querySelector(".cont-inverted");
+		contenedorManoOpponent!.style.transform = "translateY(50px)";
+		contenedorManoOpponent!.style.transform = "rotate(360deg);";
+		contenedorManoOpponent!.style.transition = "all 1s ease-in-out";
 		if (contador < 1) {
 			clearInterval(intervalo);
 			const button = div.querySelector(".button");
@@ -31,51 +34,56 @@ export async function initPageResult(params) {
 	if (gameState.owner == true) {
 		await state.whoWins(gameState.play, gameState.opponentPlay);
 		div.innerHTML = `
-    	<div class="contenedor-manos inverted">
-      	    <hand-el class="hand" contexto="ingame" hand="${gameState.opponentPlay}"></hand-el>
-      	</div>
-      	<div class="contenedor-manos base">
-      	    <hand-el class="hand" contexto="ingame" hand="${gameState.play}"></hand-el>
-      	</div>
-      	<div class="contenedor-resultado">
-      	  	<div class="resultado-posta">
-      	    	<h1>${gameState.lastGameOwnerResult}</h1>
-      	  	</div>
-      	  	<div class="resultado-score">
-      	   	 	<h2 class="resultado-score-text">Score</h2>
-      	    	<p>Vos: ${scoreboard.owner}</p>
-      	    	<p>Rival: ${scoreboard.guest}</p>
-      	  	</div>
-      	  	<div class="button-volver-a-jugar">
-      	    	<button-el class="button" label="Volver a jugar"></button-el>
-      	  	</div>
-				</div>
-				`;
+		<div class="cont-inverted">
+			<div class="contenedor-manos inverted">
+				<hand-el class="hand" contexto="ingame" hand="${gameState.opponentPlay}"></hand-el>
+			</div>
+		</div>
+		
+		<div class="contenedor-manos base">
+			<hand-el class="hand" contexto="ingame" hand="${gameState.play}"></hand-el>
+		</div>
+		<div class="contenedor-resultado">
+			<div class="resultado-posta">
+				<h1>${gameState.lastGameOwnerResult}</h1>
+			</div>
+			<div class="resultado-score">
+				<h2 class="resultado-score-text">Score</h2>
+				<p>Vos: ${scoreboard.owner}</p>
+				<p>Rival: ${scoreboard.guest}</p>
+			</div>
+			<div class="button-volver-a-jugar">
+			<button-el class="button" label="Volver a jugar"></button-el>
+			</div>
+		</div>
+		`;
 		return div;
 	}
 	if (gameState.owner == false) {
 		await state.whoWins(gameState.opponentPlay, gameState.play);
 		div.innerHTML = `
-    	<div class="contenedor-manos inverted">
-      	    <hand-el class="hand" contexto="ingame" hand="${gameState.opponentPlay}"></hand-el>
-      	</div>
-      	<div class="contenedor-manos base">
-      	    <hand-el class="hand" contexto="ingame" hand="${gameState.play}"></hand-el>
-      	</div>
-      	<div class="contenedor-resultado">
-      	  	<div class="resultado-posta">
-      	    	<h1>${gameState.lastGameGuestResult}</h1>
-      	  	</div>
-      	  	<div class="resultado-score">
-      	   	 	<h2 class="resultado-score-text">Score</h2>
-      	    	<p>Vos: ${scoreboard.guest}</p>
-      	    	<p>Rival: ${scoreboard.owner}</p>
-      	  	</div>
-      	  	<div class="button-volver-a-jugar">
-      	    	<button-el class="button" label="Volver a jugar"></button-el>
-      	  	</div>
-      	</div>
-    	`;
+		<div class="cont-inverted">
+			<div class="contenedor-manos inverted">
+				<hand-el class="hand" contexto="ingame" hand="${gameState.opponentPlay}"></hand-el>
+			</div>
+		</div>
+		<div class="contenedor-manos base">
+			<hand-el class="hand" contexto="ingame" hand="${gameState.play}"></hand-el>
+		</div>
+		<div class="contenedor-resultado">
+			<div class="resultado-posta">
+				<h1>${gameState.lastGameGuestResult}</h1>
+			</div>
+			<div class="resultado-score">
+				<h2 class="resultado-score-text">Score</h2>
+				<p>Vos: ${scoreboard.guest}</p>
+				<p>Rival: ${scoreboard.owner}</p>
+			</div>
+			<div class="button-volver-a-jugar">
+				<button-el class="button" label="Volver a jugar"></button-el>
+			</div>
+		</div>
+		`;
 		return div;
 	}
 	/*  */
